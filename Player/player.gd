b9e5f75461b0b346f8c2dc9f60a3581102aecf0d
landita03 <omarlandita03@gmail.com>
2026-07-07 16:19:34 -0600

    empeze el air moovment

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 #Player walking speed
 @export var max_walking_speed: float = 14 
-var walking_speed: float = 0 
+var walking_speed: float = 0
+@export var ari_controll: float = 0.8 
 #The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration: float = 50
 @export var jump_strength: float = 20
@@ -36,6 +37,11 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		
+		#target_velocity.x = direction.x * walking_speed / ari_controll
+		#target_velocity.z = direction.z * walking_speed / ari_controll
+		target_velocity.x = self.velocity.x + direction.x * walking_speed * ari_controll / 10
+		target_velocity.z = self.velocity.z + direction.z * walking_speed * ari_controll / 10
 	else :
 		target_velocity.y = 0
 		remaining_jumps = max_jumps
@@ -49,8 +55,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 
-#func _process(delta: float) -> void:
-	#print(self.velocity.length())
+func _process(delta: float) -> void:
+	print(self.velocity.length())
 	#print(self.is_on_floor())
 
 func _ready():
@@ -58,7 +64,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
-	print(event is InputEventMouseMotion)
+	#print(event is InputEventMouseMotion)
 	if event is InputEventMouseMotion:
 		var camera_rotation = event.relative * camera_rotation_sensitivity
 		self.rotate(Vector3.DOWN, deg_to_rad(camera_rotation.x))
