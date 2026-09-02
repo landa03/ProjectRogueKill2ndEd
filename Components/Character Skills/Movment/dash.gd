9bcb92e35bfd15d361bc5b98ca9c6ec7465c892d
landita03 <omarlandita03@gmail.com>
2026-09-01 19:41:04 -0600

    avanse a las skills

--- Components/Character Skills/Movment/dash.gd
+++ Components/Character Skills/Movment/dash.gd
@@ -1,11 +1,3 @@
-#TODO : dash
-#	stamina
-#		costo, current & max
-#	timeline
-#		duration
-#	last direction input
-#	dash speed
-
 extends CharacterSkill
 
 @export var dash_speed : float
@@ -13,4 +5,4 @@ var dash_direction: Vector3 = Vector3(0,0,-1)
 
 func _physics_process(delta: float) -> void:
 	if self.is_skill_active :
-		self.skill_current_duration
+		self.skill_owner.move_and_slide()
--- Components/Character Skills/base_character_skill.gd
+++ Components/Character Skills/base_character_skill.gd
@@ -2,6 +2,7 @@ class_name CharacterSkill
 extends Node
 
 signal skill_activated
+signal skill_finished
 
 enum SkillCategory {UNKNOWN,MOVMENT}
 @export var skill_category : SkillCategory = SkillCategory.UNKNOWN
@@ -17,19 +18,28 @@ var current_cooldown : float = cooldown
 @export var cost_per_use : float = 1
 @export var cost_per_second : float = 0
 
-@export var max_use_charges : = 1
-var current_use_charges : = max_use_charges
+@export var max_use_charges : int = 1
+var current_use_charges : int = max_use_charges
 
 @export var is_skill_active : float = false
 
-@export var skill_owner : PhysicsBody3D
+@export var skill_owner : CharacterBody3D
 
 func activate_skill():
+	current_cooldown = 0
+	skill_current_duration = 0
 	current_use_charges -= 1
 	is_skill_active = true
 	skill_activated.emit()
-	current_cooldown = 0
 	
 func _physics_process(delta: float) -> void:
-	if current_cooldown < cooldown :
+	if current_cooldown < cooldown and current_use_charges < max_use_charges :
 		current_cooldown += 1
+	elif current_cooldown > cooldown and current_use_charges < max_use_charges:
+		current_use_charges += 1
+		current_cooldown = 0
+		
+	if is_skill_active and skill_current_duration < skill_max_duration:
+		skill_current_duration += 1
+	else :
+		skill_finished.emit()
